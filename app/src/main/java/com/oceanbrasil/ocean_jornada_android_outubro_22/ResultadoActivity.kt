b0be7416a526c82eca521a4508403a576fb8ced5
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-13 10:31:21 -0300

    Funcionamento do botão `Voltar`

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/ResultadoActivity.kt
@@ -1,11 +1,18 @@
 package com.oceanbrasil.ocean_jornada_android_outubro_22
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btVoltar = findViewById<Button>(R.id.btVoltar)
+
+        btVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
