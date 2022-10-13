40aec86754a2199ea5d33fb029d9bf343badd66f
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-13 11:24:37 -0300

    Imports do `AlertDialog` comentados

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/ResultadoActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/ResultadoActivity.kt
@@ -1,11 +1,11 @@
 package com.oceanbrasil.ocean_jornada_android_outubro_22
 
-import android.content.DialogInterface
+//import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+//import android.widget.Toast
+//import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultadoActivity : AppCompatActivity() {
