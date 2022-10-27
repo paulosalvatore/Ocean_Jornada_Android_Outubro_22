d1c2a835fd6fcbac9a1eed44db858dc79288df4c
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-27 10:16:13 -0300

    Correção do `HomeActivity`, removendo referências de `nav`

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/view/HomeActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/view/HomeActivity.kt
@@ -38,7 +38,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
