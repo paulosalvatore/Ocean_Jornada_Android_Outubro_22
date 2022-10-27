981e5e1cac6a39d45f64e86bab7c539a061a077d
Paulo Salvatore <salvatoregames@gmail.com>
2022-10-27 10:58:59 -0300

    Organização inicial do navigation

--- app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/view/HomeActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_jornada_android_outubro_22/view/HomeActivity.kt
@@ -38,7 +38,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home
+                R.id.nav_criaturas
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
